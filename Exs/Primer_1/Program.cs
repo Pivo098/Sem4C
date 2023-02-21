5f40096e4126167abd6c8380b02e1c251eae972b
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-21 21:47:08 +0300

    Семинар 4

---
+++ Exs/Primer_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Напишите программу, которая принимает
+//  на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ImputInt(string prompt)
+{
+    System.Console.WriteLine($"{prompt}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool ValidatePositive(int value)
+{
+    if (value <= 0)
+    {
+        System.Console.WriteLine("Введите положительное число");
+        return false;
+    }
+    return true;
+}
+
+int SumNumbers(int value)
+{
+    int sum = 0;
+    for(int i = 1; i <= value; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+int SumGaussNambers(int value)
+{
+    return  (1 + value) * value / 2;
+}
+
+int value = ImputInt("Введите число");
+if (ValidatePositive(value))
+{
+    int sum = SumNumbers(value);
+    System.Console.WriteLine($"Сумма чисел от 1 до {value} равна {sum}");
+    System.Console.WriteLine($"Сумма чисел по Гаусу от 1 до {value} равна {SumGaussNambers(value)}");
+}
---
+++ Exs/Primer_5/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] FillArray(int size)  // метод заполнения массива, параметр метода является размером массива
+{
+    int[] myArray = new int[size]; // объявление и иницифлизация массива
+    for (int i = 0; i < myArray.Length; i++) // цикл перебора массива
+    {
+        myArray[i] = new Random().Next(0, 7); // кладем в каждый элемет массива рандомное число от 0 до 1
+    }
+    return myArray; //  возвращаем массив
+}
+
+void PrintArray(int[] array) // метод вывода массива
+{
+    for (int i = 0; i < array.Length; i++) // цикл перебора массива
+    {
+        System.Console.Write($"{array[i]}, "); // на каждой итерации цикла мы будем выводить элемент массива
+    }
+    System.Console.WriteLine(); // делается для того чтобы отступить одну строку
+}
+
+int InputSizeArray(string msg) // метод ввода числа
+{
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int sizeArray = InputSizeArray("Введите размер массива"); // вводим количество элементов массива и кладем в переменную
+int[] myArray = FillArray(sizeArray); // заполняем массив рандомным числом от 0 до 1
+PrintArray(myArray); // выводим массив
